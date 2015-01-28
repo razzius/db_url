c643539da7b1d95677c8e577ba7f0a92a862fe69
Razzi Abuissa <razzi53@gmail.com>
2015-01-28 12:41:46 -0500

    Defer work to environment variables

--- database_url_cli.py
+++ database_url_cli.py
@@ -1,69 +1,86 @@
 #!/usr/bin/env python2
 
 import os
-from urlparse import urlparse
+import sys
+import subprocess
+import urlparse
 
-import click
+
+def get_connection_command(scheme, user, database):
+    """Returns the token list of the base command to connect to a database."""
+    if scheme == 'postgres':
+        return ['psql']
+    elif scheme == 'mysql':
+        return ['mysql', '-u', user, '-D', database]
+    elif scheme == 'sqlite':
+        return ['sqlite3', database]
+    else:
+        return None
 
 
-CONNECTION_COMMANDS = {
-    'postgres': 'PGPASSWORD={password} psql -U {username} -h {host} -d {database}',
-    'mysql': 'MYSQL_PWD={password} mysql -u {username} -h {host} -D {database}',
-    'sqlite': 'sqlite3 {database}'
-}
+def get_database_env_vars(scheme, user, password, host, port, database):
+    """Returns the environment variables to set to connect to a database."""
+    if scheme == 'postgres':
+        env_vars = {
+            'PGUSER': user,
+            'PGPASSWORD': password,
+            'PGHOST': host,
+            'PGDATABASE': database
+        }
+
+        if port:
+            env_vars['PGPORT'] = str(port)
+
+        return env_vars
+    elif scheme == 'mysql':
+        env_vars = {
+            'MYSQL_PWD': password,
+            'MYSQL_HOST': host,
+        }
+
+        if port:
+            env_vars['MYSQL_TCP_PORT'] = str(port)
+
+        return env_vars
+    else:
+        return {}
 
 def parse_db_url(database_url):
-    """Parses the database_url and returns the command to connect to it."""
+    """Return a base command list and list of environment variables tuples to connect to a database_url."""
+    parsed_url = urlparse.urlparse(database_url)
 
-    # In-memory sqlite is a special case when it has this format
-    if database_url == 'sqlite://:memory:':
-        return 'sqlite3'
-
-    parsed_url = urlparse(database_url)
     scheme = parsed_url.scheme
+    user = parsed_url.username
+    database = parsed_url.path[1:]
 
-    command_format = CONNECTION_COMMANDS.get(scheme, None)
-    if not command_format:
+    database_command = get_connection_command(scheme, user, database)
+    if not database_command:
         raise ValueError('Scheme "{}" not recognized.'.format(scheme))
 
-    if scheme in ['postgres', 'mysql']:
-        scheme_valid = all((
-            parsed_url.username,
-            parsed_url.password,
-            parsed_url.hostname,
-            parsed_url.path
-        ))
-    else:
-        scheme_valid = bool(command_format)
-
-    if scheme_valid:
-        return command_format.format(username=parsed_url.username,
-                                     password=parsed_url.password,
-                                     host=parsed_url.hostname,
-                                     database=parsed_url.path[1:])
-    else:
-        raise ValueError('DATABASE_URL is invalid.')
+    return database_command, get_database_env_vars(scheme,
+                                                   user,
+                                                   parsed_url.password,
+                                                   parsed_url.hostname,
+                                                   parsed_url.port,
+                                                   database)
 
 
-@click.command()
-@click.argument('database_url', envvar='DATABASE_URL')
-@click.option('-args', 'db_args', help='Extra connection arguments', envvar='DB_ARGS')
-@click.option('-command', 'db_command', help='Command to run on the database.')
-def connect_to_database(database_url, db_args, db_command):
-    """Connect to the DATABASE_URL argument, or environment variable."""
+def connect_to_database():
+    """Connect to the DATABASE_URL environment variable."""
+    database_url = os.environ.get('DATABASE_URL', None)
+    if not database_url:
+        print('Error: set the DATABASE_URL environment variable')
+
     try:
-        connection_command = parse_db_url(database_url)
+        base_connection_command, env_vars = parse_db_url(database_url)
     except ValueError as e:
         print('Error: {}'.format(e))
         exit(1)
 
-    if db_args:
-        connection_command += ' {} '.format(db_args)
+    os.environ.update(env_vars)
 
-    if db_command:
-        connection_command += " -c '{}'".format(db_command)
-
-    os.system(connection_command)
+    command = base_connection_command + sys.argv[1:]
+    subprocess.call(command)
 
 
 if __name__ == '__main__':
